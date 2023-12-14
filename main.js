fd68fbe0aedcd21104449a67ecdf9ecdf12bb170
gio-vardana <g.vardanashvili@gmail.com>
2023-12-15 00:04:10 +0400

    crosby

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 const burger = document.querySelector('.burger')
 const nav = document.querySelector('nav')
-
+console.log(burger)
 
 function triggerMenu() {
     burger.classList.toggle('active')
     nav.classList.toggle('active')
 }
-
