5a1270247e26889690359052eea00f97342f035a
gio-vardana <g.vardanashvili@gmail.com>
2023-12-14 23:56:53 +0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+
+document.querySelector('.menu-icon').addEventListener('click', function () {
+    this.classList.toggle('active')
+    document.querySelector('.menu-items').classList.toggle('active')
+  })
+  
--- main.js
+++
@@ -1,9 +0,0 @@
-const burger = document.querySelector('.burger')
-const nav = document.querySelector('nav')
-
-
-function triggerMenu() {
-    burger.classList.toggle('active')
-    nav.classList.toggle('active')
-}
-
