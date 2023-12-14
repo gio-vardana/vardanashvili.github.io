1afd576f61c0158aad96848fb893a91dae224d91
gio-vardana <g.vardanashvili@gmail.com>
2023-12-15 00:19:40 +0400

    add

--- main.js
+++
@@ -1,8 +0,0 @@
-const burger = document.querySelector('.burger')
-const nav = document.querySelector('nav')
-console.log(burger)
-
-function triggerMenu() {
-    burger.classList.toggle('active')
-    nav.classList.toggle('active')
-}
